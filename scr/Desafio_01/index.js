bf228a3c2362e4219021172619acd37f1248047f
FelipeTrakinas <fetelesfb@gmail.com>
2026-09-20 19:37:28 -0300

    Realizando desafio DIO - Nome e nivel do Herói

---
+++ scr/Desafio_01/index.js
@@ -0,0 +1,31 @@
+let nomeDoHeroi = "Felipe";
+let xp = 6001;
+let nivel = "";
+
+function calcularNivel(xp) {
+if (xp <= 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp < 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp < 5000) {
+  nivel = "Prata";
+} else if (xp >= 5001 && xp < 6000) {
+  nivel = "Ouro";
+} else if (xp >= 6001 && xp < 7000) {
+  nivel = "Diamante";
+} else if (xp >= 7001 && xp < 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp < 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp < 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+}
+
+
+
+calcularNivel(xp);
+console.log("O Herói de nome " + nomeDoHeroi + " está no nível " + nivel);
+
